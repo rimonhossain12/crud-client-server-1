5e1484b9ef8c80bf1ee54dd846c9efee73639dfe
rimonspi12 <rimonmpi@gmail.com>
2021-11-28 11:34:13 +0600

    react js client server is added

---
+++ src/Pages/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+           {/*  <Link></Link> */}
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Pages/UpdatedUser/UpdatedUser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const f = () => {
+    return (
+        <div>
+            <h2>This is update user</h2>
+        </div>
+    );
+};
+
+export default f;
